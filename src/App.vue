e24d6b81e82053d0c636b25eaea00817038fa0d0
Nate Bross <nate@brosstribe.com>
2025-03-10 22:36:57 -0500

    feat: remove vue template stylings

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import TheWelcome from './components/TheWelcome.vue';
+import TheWelcome from './components/QuickText.vue';
 </script>
 
 <template>
@@ -35,23 +35,5 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
