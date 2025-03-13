2cc49bf283752c5e013a82728161deeb79242afe
Nate Bross <nate@brosstribe.com>
2025-03-12 20:15:06 -0500

    fix: another try at fixing github pages issue

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     VueDevTools(),
     basicSsl()
   ],
-  base: 'fuzzzerd.github.io/quicktext/',
+  base: 'https://fuzzzerd.github.io/quicktext/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
